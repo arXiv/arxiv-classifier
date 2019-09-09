af0f3a63bc37210dc89df8650db1c024a062f568
Jaimie Murdock <jmm698@cornell.edu>
2019-09-09 15:04:34 -0600

    reorganizing classifier service

---
+++ classifier/services/classifier/__init__.py
@@ -0,0 +1,12 @@
+"""
+Calling the classifier service.
+"""
+from typing import List
+
+from classifier.domain import ClassifierPrediction
+
+import classifier.services.naivebayes
+
+def classify(content: bytes) -> List[ClassifierPrediction]:
+    """Performs a classification."""
+    
