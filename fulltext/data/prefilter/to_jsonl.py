864c4126ce0c8f244117c2d3db3be2eff042b64d
Brian Maltzan <bgm37@cornell.edu>
2021-12-16 11:25:33 -0500

    Add plain convert to jsonlines. Currently converts to GCP vertex json structure.

--- fulltext/data/prefilter/to_jsonl.py
+++ fulltext/data/prefilter/to_jsonl.py
@@ -1,7 +1,7 @@
 # File: to_jsonl.py
 # Desc: convert paper data in json to json lines
 #
-# Note: this wasn't too useful. Google's vertex-ai worked on small datasets, 
+# Note: Google's vertex-ai worked on small datasets,
 #       but then only returned an internal-error message when training on 700k papers,
 #       which is near it's 1M limit.
 #
@@ -15,36 +15,51 @@
 #       "aiplatform.googleapis.com/ml_use": "training|test|validation"
 #     }
 #   }
+#
+# Keys in the source json data sets:
+#   {"document_id":"1949936","paper_id":"2108.12839","version":"1",
+#    "yymm":"2108","primary_category":"math.CO",
+#    "title":"Restricted Positional Games","text_type":"fulltext","text": ... }
+#
+# There seems to be a few flavors of json lines, here's one:
+#   https://jsonlines.org/examples/
 
 import json
 import jsonlines
 import sys
 import stream_json
 
-if len(sys.argv) != 4:
-  print("Usage: python to_jsonl.py readfile writefile usage")
+GOOGLE_VERTEX_FORMAT=False
+
+if len(sys.argv) not in (3,4):
+  print("Usage: python to_jsonl.py readfile writefile [usage]")
   print("   eg: python to_jsonl.py ds9-mixed-all-validate.json ds9-mixed-all-validate.jsonl validation")
 
 else:
   readfile  = sys.argv[1]
   writefile = sys.argv[2]
-  usage     = sys.argv[3]
+  usage     = sys.argv[3] if len(sys.argv) == 4 else 'validation'
 
   with jsonlines.open(writefile, mode='w') as wf:
     with open(readfile) as rf:
       sj = stream_json.StreamJson(rf)
       for item in sj:
         j  = json.loads(item)
-        pc = j["primary_category"]
-        if type(j["text"]) == str:
-          ft = j["text"]
-        else:
-          ft = ','.join( j["text"] )
 
-        data = { 
-          "classificationAnnotation": { "displayName": pc }, 
-          "textContent": ft, 
-          "dataItemResourceLabels": { "aiplatform.googleapis.com/ml_use": usage }
-        }
-        wf.write(data)
+        if GOOGLE_VERTEX_FORMAT:
+          pc = j["primary_category"]
+          if type(j["text"]) == str:
+            ft = j["text"]
+          else:
+            ft = ','.join( j["text"] )
+
+          data = {
+            "classificationAnnotation": { "displayName": pc },
+            "textContent": ft,
+            "dataItemResourceLabels": { "aiplatform.googleapis.com/ml_use": usage }
+          }
+          wf.write(data)
+
+        else:
+          wf.write(j)
 
